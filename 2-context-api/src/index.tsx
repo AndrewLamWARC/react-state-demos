4d613fc8bb02dafa5404c5582661623a27899b2a
andrewlam <andrew.lam@ascential.com>
2022-10-10 11:09:24 +0100

    ContextApi: Update to react 18

--- 2-context-api/src/index.tsx
+++ 2-context-api/src/index.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react"
-import { render } from "react-dom"
+import { createRoot } from "react-dom/client"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 
-// New Root API landing in react 18. Following is deprecated and now called Legacy Root API
 const container = document.getElementById("root")
-render(
+// eslint-disable-next-line
+const root = createRoot(container!)
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  container
+  </StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
